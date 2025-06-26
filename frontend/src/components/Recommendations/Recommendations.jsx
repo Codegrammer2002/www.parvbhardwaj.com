2fea843b5a4924a2215b9b93301f3b219a1ef424
Codegrammer <bhardwajparv2015@gmail.com>
2025-06-26 16:25:32 -0400

    Updated Linkedin plugin

--- frontend/src/components/Recommendations/Recommendations.jsx
+++ frontend/src/components/Recommendations/Recommendations.jsx
@@ -12,7 +12,7 @@ const Recommendations = () => {
   return (
     <section>
       <h2 className="text-yellow-400 text-2xl font-bold mb-4">Recommendations</h2>
-      <div className="sk-ww-linkedin-recommendations" data-embed-id="25571205"></div>
+  
       <iframe src='https://widgets.sociablekit.com/linkedin-recommendations/iframe/25571205' frameborder='0' width='100%' height='1000'></iframe>
     </section>
   );
