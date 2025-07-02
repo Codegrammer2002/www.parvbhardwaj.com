c3248e5dae21828926f79f34aec61dea7c119a64
Codegrammer <bhardwajparv2015@gmail.com>
2025-07-02 12:07:04 -0400

    Fixed some errors and typos

--- frontend/src/pages/Contact/Contact.jsx
+++ frontend/src/pages/Contact/Contact.jsx
@@ -2,8 +2,8 @@
 const Contact = () => {
   return (
     <section className="min-h-screen bg-black text-white pt-24 px-6">
-      <h2 className="text-yellow-400 text-3xl font-bold mb-8 text-center">Hire/Contact Me</h2>
-      <p className="text-white-400 text-1xl font-bold mb-8 text-center">If you're looking to hire me or have any questions, you can email me by clicking the link below or connect with me on Linkedin or fill out the form! Whatever works the best for you</p> 
+      <h2 className="text-yellow-400 text-3xl font-bold mb-8 text-center">Want to hire or get in touch? Contact Me</h2>
+      <p className="text-white-400 text-1xl font-bold mb-8 text-center">If you're looking for an intern or a full-time employee, you can email me by clicking the link below or connect with me on Linkedin or fill out the form! Whatever works the best for you</p> 
       {/* Social Links */}
       <div className="flex justify-center space-x-8 mb-10">
         <a href="https://www.linkedin.com/in/parv-bhardwaj/" target="_blank" className="text-blue-400 hover:underline">LinkedIn</a>
